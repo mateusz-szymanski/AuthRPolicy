1655dfa977d15985e1a7491affb94358ae002d79
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-07 21:49:44 +0200

    Add test

--- Authorization.Tests/Tests/Services/AuthorizationServiceTests.cs
+++ Authorization.Tests/Tests/Services/AuthorizationServiceTests.cs
@@ -168,7 +168,7 @@ namespace Authorization.Tests.Tests.Services
         }
 
         [Fact]
-        public async Task IsUserAuthorized_ShouldReturnFalse_GivenUserWithProperMulitpleSubPermissionsButWithAllAccessPolicyReturnFalse()
+        public async Task IsUserAuthorized_ShouldReturnFalse_GivenUserWithProperMulitpleSubPermissionsButWithAllAccessPoliciesReturnFalse()
         {
             // Arrange
             var role1 = new Role("my-role-1");
@@ -200,6 +200,63 @@ namespace Authorization.Tests.Tests.Services
             var accessPolicy3Stub = new AccessPolicy3Stub();
             var accessPolicy3CheckerStub = new AccessPolicy3CheckerStub()
                 .ShouldReturn((_, _) => false);
+
+            var permissionAccessPolicy = new PermissionAccessPolicy(permissionMainName, accessPolicy1Stub, accessPolicy2Stub, accessPolicy3Stub);
+
+            var serviceCollection = new ServiceCollection()
+                .AddNullLogger()
+                .AddScoped<IAuthorizationService, AuthorizationService>()
+                .AddSingleton<IPermissionProvider>(permissionProviderStub)
+                .AddSingleton<IRoleProvider>(roleProviderStub)
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy1Stub>>(accessPolicy1CheckerStub)
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy2Stub>>(accessPolicy2CheckerStub)
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy3Stub>>(accessPolicy3CheckerStub);
+
+            await using var serviceProvider = serviceCollection.BuildServiceProvider();
+            await using var serviceScope = serviceProvider.CreateAsyncScope();
+
+            var authorizationService = serviceScope.ServiceProvider.GetRequiredService<IAuthorizationService>();
+
+            // Act
+            var isUserAuthorized = authorizationService.IsUserAuthorized(user, permissionAccessPolicy);
+
+            // Assert
+            Assert.False(isUserAuthorized);
+        }
+
+        [Fact]
+        public async Task IsUserAuthorized_ShouldReturnFalse_GivenUserWithProperSubPermissionButWithAccessPolicyReturnFalse()
+        {
+            // Arrange
+            var role1 = new Role("my-role-1");
+            var roleProviderStub = new RoleProviderStub()
+                .WithRoles(role1);
+
+            var permissionMainName = "my-permission";
+
+            var permission1 = new Permission<AccessPolicy1Stub>(permissionMainName, "1");
+            var permission2 = new Permission<AccessPolicy2Stub>(permissionMainName, "2");
+            var permission3 = new Permission<AccessPolicy3Stub>(permissionMainName, "3");
+
+            var permissionProviderStub = new PermissionProviderStub()
+                .WithRole(role1, permission1);
+
+            var userMock = new Mock<IUser>();
+            userMock.Setup(u => u.UserName).Returns("my-username");
+            userMock.Setup(u => u.Roles).Returns(new[] { role1 });
+            var user = userMock.Object;
+
+            var accessPolicy1Stub = new AccessPolicy1Stub();
+            var accessPolicy1CheckerStub = new AccessPolicy1CheckerStub()
+                .ShouldReturn((_, _) => false);
+
+            var accessPolicy2Stub = new AccessPolicy2Stub();
+            var accessPolicy2CheckerStub = new AccessPolicy2CheckerStub()
+                .ShouldReturn((_, _) => true);
+
+            var accessPolicy3Stub = new AccessPolicy3Stub();
+            var accessPolicy3CheckerStub = new AccessPolicy3CheckerStub()
+                .ShouldReturn((_, _) => true);
 
             var permissionAccessPolicy = new PermissionAccessPolicy(permissionMainName, accessPolicy1Stub, accessPolicy2Stub, accessPolicy3Stub);
 
