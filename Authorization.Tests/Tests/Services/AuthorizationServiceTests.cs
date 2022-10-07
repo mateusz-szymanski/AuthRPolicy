f0c61c7bf7811f08bf44fdf501eb23c2fc1904e2
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-07 21:33:28 +0200

    Extends tests

--- Authorization.Tests/Tests/Services/AuthorizationServiceTests.cs
+++ Authorization.Tests/Tests/Services/AuthorizationServiceTests.cs
@@ -14,9 +14,11 @@ namespace Authorization.Tests.Tests.Services
     public class AuthorizationServiceTests
     {
         [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public async Task IsUserAuthorized_ShouldReturnFalse_GivenUserWithoutProperPermission(bool accessPolicyHasAccess)
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public async Task IsUserAuthorized_ShouldReturnFalse_GivenUserWithoutProperPermission(bool accessPolicy1HasAccess, bool accessPolicy2HasAccess)
         {
             // Arrange
             var role1 = new Role("my-role-1");
@@ -36,7 +38,11 @@ namespace Authorization.Tests.Tests.Services
 
             var accessPolicy1Stub = new AccessPolicy1Stub();
             var accessPolicy1CheckerStub = new AccessPolicy1CheckerStub()
-                .ShouldReturn((_, _) => accessPolicyHasAccess);
+                .ShouldReturn((_, _) => accessPolicy1HasAccess);
+
+            var accessPolicy2Stub = new AccessPolicy2Stub();
+            var accessPolicy2CheckerStub = new AccessPolicy2CheckerStub()
+                .ShouldReturn((_, _) => accessPolicy2HasAccess);
 
             var permissionAccessPolicy = new PermissionAccessPolicy(actionPermission.MainName, accessPolicy1Stub);
 
@@ -45,7 +51,56 @@ namespace Authorization.Tests.Tests.Services
                 .AddScoped<IAuthorizationService, AuthorizationService>()
                 .AddSingleton<IPermissionProvider>(permissionProviderStub)
                 .AddSingleton<IRoleProvider>(roleProviderStub)
-                .AddSingleton<IAccessPolicyChecker<AccessPolicy1Stub>>(accessPolicy1CheckerStub);
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy1Stub>>(accessPolicy1CheckerStub)
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy2Stub>>(accessPolicy2CheckerStub);
+
+            await using var serviceProvider = serviceCollection.BuildServiceProvider();
+            await using var serviceScope = serviceProvider.CreateAsyncScope();
+
+            var authorizationService = serviceScope.ServiceProvider.GetRequiredService<IAuthorizationService>();
+
+            // Act
+            var isUserAuthorized = authorizationService.IsUserAuthorized(user, permissionAccessPolicy);
+
+            // Assert
+            Assert.False(isUserAuthorized);
+        }
+
+        [Fact]
+        public async Task IsUserAuthorized_ShouldReturnFalse_GivenUserWithProperPermissionButWithAccessPolicyReturnFalse()
+        {
+            // Arrange
+            var role1 = new Role("my-role-1");
+            var roleProviderStub = new RoleProviderStub()
+                .WithRoles(role1);
+
+            var permission = new Permission<AccessPolicy1Stub>("my-permission-1");
+
+            var permissionProviderStub = new PermissionProviderStub()
+                .WithRole(role1, permission);
+
+            var userMock = new Mock<IUser>();
+            userMock.Setup(u => u.UserName).Returns("my-username");
+            userMock.Setup(u => u.Roles).Returns(new[] { role1 });
+            var user = userMock.Object;
+
+            var accessPolicy1Stub = new AccessPolicy1Stub();
+            var accessPolicy1CheckerStub = new AccessPolicy1CheckerStub()
+                .ShouldReturn((_, _) => false);
+
+            var accessPolicy2Stub = new AccessPolicy2Stub();
+            var accessPolicy2CheckerStub = new AccessPolicy2CheckerStub()
+                .ShouldReturn((_, _) => false);
+
+            var permissionAccessPolicy = new PermissionAccessPolicy(permission.MainName, accessPolicy1Stub);
+
+            var serviceCollection = new ServiceCollection()
+                .AddNullLogger()
+                .AddScoped<IAuthorizationService, AuthorizationService>()
+                .AddSingleton<IPermissionProvider>(permissionProviderStub)
+                .AddSingleton<IRoleProvider>(roleProviderStub)
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy1Stub>>(accessPolicy1CheckerStub)
+                .AddSingleton<IAccessPolicyChecker<AccessPolicy2Stub>>(accessPolicy2CheckerStub);
 
             await using var serviceProvider = serviceCollection.BuildServiceProvider();
             await using var serviceScope = serviceProvider.CreateAsyncScope();
