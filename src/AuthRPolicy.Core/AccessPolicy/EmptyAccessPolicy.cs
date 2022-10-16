c02ae81c2ea448627459c8e53a8396196f30a769
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-16 22:31:21 +0200

    Add Todo

--- src/AuthRPolicy.Core/AccessPolicy/EmptyAccessPolicy.cs
+++ src/AuthRPolicy.Core/AccessPolicy/EmptyAccessPolicy.cs
@@ -1,6 +1,7 @@
 ﻿namespace AuthRPolicy.Core.AccessPolicy
 {
     // TODO: Repo name AuthRIt, AuthRPolicy, AuthRPolicy?
+    // TODO: Rename to DefaultAccessPolicy? No, not really?
     public record EmptyAccessPolicy : IAccessPolicy
     {
         public string Name => "Empty";
