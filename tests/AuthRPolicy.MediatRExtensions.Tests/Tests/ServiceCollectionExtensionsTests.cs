b078e2fc8de4f855c15a1607a2e615681671ca85
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-23 20:02:53 +0200

    Add AuthRPolicy.MediatRExtensions.Tests

---
+++ tests/AuthRPolicy.MediatRExtensions.Tests/Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using AuthRPolicy.MediatRExtensions.IoC;
+using AuthRPolicy.MediatRExtensions.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace AuthRPolicy.MediatRExtensions.Tests.Tests
+{
+    public class ServiceCollectionExtensionsTests
+    {
+        [Fact]
+        public void AddAuthorizationBehavior_ShouldRegisterServices_GivenNullRoleProviderAndEmptyAssembly()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddAuthorizationBehavior();
+
+            // Assert
+            Assert.Contains(services, sd =>
+                sd.ServiceType == typeof(ICurrentUserService) &&
+                sd.ImplementationType == typeof(HttpContextBasedCurrentUserService) &&
+                sd.Lifetime == ServiceLifetime.Scoped);
+
+            Assert.Contains(services, sd =>
+                sd.ServiceType == typeof(IPipelineBehavior<,>) &&
+                sd.ImplementationType == typeof(AuthorizationBehavior<,>) &&
+                sd.Lifetime == ServiceLifetime.Scoped);
+        }
+    }
+}
