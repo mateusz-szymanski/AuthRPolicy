3c5a9ec1a68d7904fb7006fcf13bdf734c396bc3
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-23 20:56:36 +0200

    AuthorizationBehaviorTests WIP

---
+++ tests/AuthRPolicy.MediatRExtensions.Tests/Tests/AuthorizationBehaviorTests.cs
@@ -0,0 +1,53 @@
+﻿using AuthRPolicy.Core;
+using AuthRPolicy.Core.Permissions;
+using AuthRPolicy.Core.Services;
+using AuthRPolicy.MediatRExtensions.Services;
+using MediatR;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuthRPolicy.MediatRExtensions.Tests.Tests
+{
+    // TODO: 
+    public class AuthorizationBehaviorTests
+    {
+        public record AnonymousRequest : IRequest { }
+        //public record AuthorizedRequest : IAuthorizedRequest
+        //{
+        //    public PermissionAccessPolicy PermissionAccessPolicy => new PermissionAccessPolicy("my-permission", ...);
+        //}
+
+        [Fact]
+        public async Task Handle_ShouldRunHandler_GivenAnonymousRequest()
+        {
+            // Arrange
+            var authorizationServiceMock = new Mock<IAuthorizationService>();
+            var currentUserServiceMock = new Mock<ICurrentUserService>();
+
+            var logger = new NullLogger<AuthorizationBehavior<AnonymousRequest, Unit>>();
+            var authorizationBehavior = new AuthorizationBehavior<AnonymousRequest, Unit>(
+                logger,
+                authorizationServiceMock.Object,
+                currentUserServiceMock.Object);
+
+            var request = new AnonymousRequest();
+            var handled = false;
+
+            RequestHandlerDelegate<Unit> requestHandlerDelegate = () =>
+            {
+                handled = true;
+                return Unit.Task;
+            };
+
+            // Act
+            await authorizationBehavior.Handle(request, requestHandlerDelegate, default);
+
+            // Assert
+            Assert.True(handled);
+            authorizationServiceMock.Verify(s => s.GetUserPermissions(It.IsAny<User>()), Times.Never);
+            authorizationServiceMock.Verify(s => s.IsUserAuthorized(It.IsAny<User>(), It.IsAny<PermissionAccessPolicy>()), Times.Never);
+        }
+    }
+}
