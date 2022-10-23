f24c6f18179d81ce4137fb08f6bdbf3f6539fa7e
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-23 21:24:11 +0200

    Extend AuthorizationBehaviorTests

--- tests/AuthRPolicy.MediatRExtensions.Tests/Tests/AuthorizationBehaviorTests.cs
+++ tests/AuthRPolicy.MediatRExtensions.Tests/Tests/AuthorizationBehaviorTests.cs
@@ -1,23 +1,28 @@
 ﻿using AuthRPolicy.Core;
+using AuthRPolicy.Core.AccessPolicy;
 using AuthRPolicy.Core.Permissions;
+using AuthRPolicy.Core.Roles;
 using AuthRPolicy.Core.Services;
+using AuthRPolicy.MediatRExtensions.Exceptions;
 using AuthRPolicy.MediatRExtensions.Services;
 using MediatR;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace AuthRPolicy.MediatRExtensions.Tests.Tests
 {
-    // TODO: 
     public class AuthorizationBehaviorTests
     {
         public record AnonymousRequest : IRequest { }
-        //public record AuthorizedRequest : IAuthorizedRequest
-        //{
-        //    public PermissionAccessPolicy PermissionAccessPolicy => new PermissionAccessPolicy("my-permission", ...);
-        //}
+        public record AuthorizedRequest : IRequest, IAuthorizedRequest
+        {
+            public PermissionAccessPolicy PermissionAccessPolicy { get; }
+
+            public AuthorizedRequest(PermissionAccessPolicy permissionAccessPolicy) => PermissionAccessPolicy = permissionAccessPolicy;
+        }
 
         [Fact]
         public async Task Handle_ShouldRunHandler_GivenAnonymousRequest()
@@ -46,8 +51,87 @@ namespace AuthRPolicy.MediatRExtensions.Tests.Tests
 
             // Assert
             Assert.True(handled);
-            authorizationServiceMock.Verify(s => s.GetUserPermissions(It.IsAny<User>()), Times.Never);
+            currentUserServiceMock.Verify(s => s.GetCurrentUser(It.IsAny<CancellationToken>()), Times.Never);
             authorizationServiceMock.Verify(s => s.IsUserAuthorized(It.IsAny<User>(), It.IsAny<PermissionAccessPolicy>()), Times.Never);
+            authorizationServiceMock.Verify(s => s.GetUserPermissions(It.IsAny<User>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldRunHandler_GivenAuthorizedRequestAndIsAuthorizedUser()
+        {
+            // Arrange
+            var user = new User("my-userName", new Role[0]);
+
+            var authorizationServiceMock = new Mock<IAuthorizationService>();
+            authorizationServiceMock.Setup(asm => asm.IsUserAuthorized(It.IsAny<User>(), It.IsAny<PermissionAccessPolicy>())).Returns(true);
+
+            var currentUserServiceMock = new Mock<ICurrentUserService>();
+            currentUserServiceMock.Setup(cusm => cusm.GetCurrentUser(It.IsAny<CancellationToken>())).ReturnsAsync(user);
+
+            var logger = new NullLogger<AuthorizationBehavior<AuthorizedRequest, Unit>>();
+            var authorizationBehavior = new AuthorizationBehavior<AuthorizedRequest, Unit>(
+                logger,
+                authorizationServiceMock.Object,
+                currentUserServiceMock.Object);
+
+            var permissionAccessPolicy = new PermissionAccessPolicy("permission-main-name", new EmptyAccessPolicy());
+
+            var request = new AuthorizedRequest(permissionAccessPolicy);
+            var handled = false;
+
+            RequestHandlerDelegate<Unit> requestHandlerDelegate = () =>
+            {
+                handled = true;
+                return Unit.Task;
+            };
+
+            // Act
+            await authorizationBehavior.Handle(request, requestHandlerDelegate, default);
+
+            // Assert
+            Assert.True(handled);
+            currentUserServiceMock.Verify(s => s.GetCurrentUser(It.IsAny<CancellationToken>()), Times.Once);
+            authorizationServiceMock.Verify(s => s.IsUserAuthorized(user, permissionAccessPolicy), Times.Once);
+            authorizationServiceMock.Verify(s => s.GetUserPermissions(It.IsAny<User>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowUserUnauthorizedException_GivenAuthorizedRequestAndIsUnauthorizedUser()
+        {
+            // Arrange
+            var user = new User("my-userName", new Role[0]);
+
+            var authorizationServiceMock = new Mock<IAuthorizationService>();
+            authorizationServiceMock.Setup(asm => asm.IsUserAuthorized(It.IsAny<User>(), It.IsAny<PermissionAccessPolicy>())).Returns(false);
+
+            var currentUserServiceMock = new Mock<ICurrentUserService>();
+            currentUserServiceMock.Setup(cusm => cusm.GetCurrentUser(It.IsAny<CancellationToken>())).ReturnsAsync(user);
+
+            var logger = new NullLogger<AuthorizationBehavior<AuthorizedRequest, Unit>>();
+            var authorizationBehavior = new AuthorizationBehavior<AuthorizedRequest, Unit>(
+                logger,
+                authorizationServiceMock.Object,
+                currentUserServiceMock.Object);
+
+            var permissionAccessPolicy = new PermissionAccessPolicy("permission-main-name", new EmptyAccessPolicy());
+
+            var request = new AuthorizedRequest(permissionAccessPolicy);
+            var handled = false;
+
+            RequestHandlerDelegate<Unit> requestHandlerDelegate = () =>
+            {
+                handled = true;
+                return Unit.Task;
+            };
+
+            // Act
+            await Assert.ThrowsAsync<UserUnauthorizedException>(() => authorizationBehavior.Handle(request, requestHandlerDelegate, default));
+
+            // Assert
+            Assert.False(handled);
+            currentUserServiceMock.Verify(s => s.GetCurrentUser(It.IsAny<CancellationToken>()), Times.Once);
+            authorizationServiceMock.Verify(s => s.IsUserAuthorized(user, permissionAccessPolicy), Times.Once);
+            authorizationServiceMock.Verify(s => s.GetUserPermissions(It.IsAny<User>()), Times.Never);
         }
     }
 }
