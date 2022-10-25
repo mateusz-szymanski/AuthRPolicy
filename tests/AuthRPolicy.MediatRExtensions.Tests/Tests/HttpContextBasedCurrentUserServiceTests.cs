d0cdeb44c70edff783f0987ca1b59dc0b0442dcb
Mateusz <mateuszd.szymanski@gmail.com>
2022-10-25 23:00:14 +0200

    Expand HttpContextBasedCurrentUserServiceTests

--- tests/AuthRPolicy.MediatRExtensions.Tests/Tests/HttpContextBasedCurrentUserServiceTests.cs
+++ tests/AuthRPolicy.MediatRExtensions.Tests/Tests/HttpContextBasedCurrentUserServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using AuthRPolicy.MediatRExtensions.Services;
+﻿using AuthRPolicy.Core.Roles;
+using AuthRPolicy.MediatRExtensions.Exceptions;
+using AuthRPolicy.MediatRExtensions.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
@@ -11,7 +13,6 @@ namespace AuthRPolicy.MediatRExtensions.Tests.Tests
 {
     public class HttpContextBasedCurrentUserServiceTests
     {
-        // TODO:
         [Fact]
         public async Task GetCurrentUser_ShouldReturnUser_GivenIdentityWithUserNameClaimOnly()
         {
@@ -42,6 +43,79 @@ namespace AuthRPolicy.MediatRExtensions.Tests.Tests
             // Assert
             Assert.Equal(userName, currentUser.UserName);
             Assert.Empty(currentUser.Roles);
+        }
+
+        [Fact]
+        public async Task GetCurrentUser_ShouldReturnUser_GivenIdentityWithUserNameAndRolesClaims()
+        {
+            // Arrange
+            var userName = "user-name-01";
+            var role1 = new Role("role-1");
+            var role2 = new Role("role-2");
+
+            var claims = new Claim[]
+            {
+                new Claim("preferred_username", userName),
+                new Claim("role", role1.Name),
+                new Claim("role", role2.Name)
+            };
+
+            var identity = new ClaimsIdentity(claims);
+            var contextUser = new ClaimsPrincipal(identity);
+
+            var httpContext = new DefaultHttpContext()
+            {
+                User = contextUser
+            };
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(hcam => hcam.HttpContext).Returns(httpContext);
+
+            var logger = new NullLogger<HttpContextBasedCurrentUserService>();
+
+            var httpContextBasedCurrentUserService = new HttpContextBasedCurrentUserService(httpContextAccessorMock.Object, logger);
+
+            // Act
+            var currentUser = await httpContextBasedCurrentUserService.GetCurrentUser(CancellationToken.None);
+
+            // Assert
+            Assert.Equal(userName, currentUser.UserName);
+
+            var expectedRoles = new[] { role1, role2 };
+            Assert.Equal(expectedRoles, currentUser.Roles);
+        }
+
+        [Fact]
+        public async Task GetCurrentUser_ShouldThrowMissingUserNameClaimException_GivenIdentityWithoutUserName()
+        {
+            // Arrange
+            var role1 = new Role("role-1");
+            var role2 = new Role("role-2");
+
+            var claims = new Claim[]
+            {
+                new Claim("role", role1.Name),
+                new Claim("role", role2.Name)
+            };
+
+            var identity = new ClaimsIdentity(claims);
+            var contextUser = new ClaimsPrincipal(identity);
+
+            var httpContext = new DefaultHttpContext()
+            {
+                User = contextUser
+            };
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(hcam => hcam.HttpContext).Returns(httpContext);
+
+            var logger = new NullLogger<HttpContextBasedCurrentUserService>();
+
+            var httpContextBasedCurrentUserService = new HttpContextBasedCurrentUserService(httpContextAccessorMock.Object, logger);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<MissingUserNameClaimException>(
+                async () => await httpContextBasedCurrentUserService.GetCurrentUser(CancellationToken.None)
+            );
         }
     }
 }
